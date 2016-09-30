3c4d0b3313f4e9cdf194d983690fda8cc8c42a80
silencewwt <silencewwt@gmail.com>
2016-09-30 13:51:37 +0800

    Invert binary tree

---
+++ 226-Invert-Binary-Tree.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+
+    def invertTree2(self, root):
+        if not root:
+            return root
+        queue = [root]
+        while queue:
+            node = queue.pop(0)
+            node.left, node.right = node.right, node.left
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return root
