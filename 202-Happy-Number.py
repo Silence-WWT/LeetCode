7cdb8a15bd0b078448eeadbf53b0803096f2af3a
silencewwt <silencewwt@gmail.com>
2016-10-10 14:57:41 +0800

    Happy number

--- 202-Happy-Number.py
+++ 202-Happy-Number.py
@@ -7,9 +7,22 @@ class Solution(object):
         """
         nums = {n}
         while True:
-            n = sum(map(lambda x: int(x) * int(x), str(n)))
+            n = sum(int(i) ** 2 for i in str(n))
             if n == 1:
                 return True
             elif n in nums:
                 return False
             nums.add(n)
+
+    def isHappy2(self, n):
+        def get_sum(n):
+            return sum(int(i) ** 2 for i in str(n))
+
+        slow = get_sum(n)
+        fast = get_sum(get_sum(n))
+        while slow != fast:
+            slow = get_sum(slow)
+            fast = get_sum(get_sum(fast))
+        if slow == 1:
+            return True
+        return False
